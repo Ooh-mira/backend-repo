31ce56952c76a3aa97c7964b5fe51852e57596e7
junnukim1007gmail.com <junnukim1007@gmail.com>
2026-01-12 13:21:03 +0900

    feat: init repo

---
+++ src/main/java/com/junwoo/catchtablebackend/CatchtableBackendApplication.java
@@ -0,0 +1,13 @@
+package com.junwoo.catchtablebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchtableBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchtableBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/junwoo/catchtablebackend/CatchtableBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junwoo.catchtablebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchtableBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
