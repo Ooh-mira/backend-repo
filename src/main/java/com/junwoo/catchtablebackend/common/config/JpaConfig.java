255fb4a2b311beba487f9515b3b91d652d8159aa
junnukim1007gmail.com <junnukim1007@gmail.com>
2026-01-15 16:34:24 +0900

    settings: 예외 처리 및 공통 엔티티 설정

---
+++ src/main/java/com/junwoo/catchtablebackend/common/config/JpaConfig.java
@@ -0,0 +1,14 @@
+package com.junwoo.catchtablebackend.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ *
+ * @author junnukim1007gmail.com
+ * @date 26. 1. 15.
+ */
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
---
+++ src/main/java/com/junwoo/catchtablebackend/common/entity/UpdatableBaseEntity.java
@@ -0,0 +1,25 @@
+package com.junwoo.catchtablebackend.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author junnukim1007gmail.com
+ * @date 26. 1. 15.
+ */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@MappedSuperclass
+public class UpdatableBaseEntity extends BaseEntity{
+
+    @LastModifiedDate
+    @Column
+    private LocalDateTime updatedAt;
+}
