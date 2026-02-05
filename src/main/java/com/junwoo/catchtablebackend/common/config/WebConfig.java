ae475a308fa93f5b1d2e77d6352549ca11da86da
JungMinB7 <ljlm0829@gmail.com>
2026-02-05 14:48:55 +0900

    feat: WebConfig 설정

---
+++ src/main/java/com/junwoo/catchtablebackend/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.junwoo.catchtablebackend.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://localhost:3000") // Next.js 서버 주소 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
+                .allowedHeaders("*")
+                .allowCredentials(true); // 쿠키/인증 정보 포함 허용
+    }
+}
